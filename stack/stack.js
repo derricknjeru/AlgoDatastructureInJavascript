38e6ff2f2acf97e259d21cfdad660ff27f1550ee
derrick <derricknjeru5@gmail.com>
2022-04-18 10:15:48 +0300

    implement stack

---
+++ stack/stack.js
@@ -0,0 +1,48 @@
+class Stack {
+  constructor() {
+    this.items = {}; //object or Array
+    this.count = 0;
+  }
+
+  push(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  //return and remove the top element
+  //return undefined if stack is empty
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    }
+
+    this.count--;
+    let topElement = this.items[this.count];
+    delete this.items[this.count];
+    return topElement;
+  }
+
+  peek() {
+    return this.items[this.count - 1];
+  }
+
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return this.count === 0;
+  }
+}
+
+const stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.size());
+console.log(stack.pop());
+console.log(stack.isEmpty());
